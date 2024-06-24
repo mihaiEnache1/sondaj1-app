c08bbc5c4c1fdeac6d9f6390032f2a920236c733
Enache Mihai <mihaienache51@yahoo.com>
2024-06-24 22:29:39 +0300

    Added functionality for multiple surveys

--- app.py
+++ app.py
@@ -21,91 +21,101 @@ storage_client = storage.Client()
 bucket_name = 'sondaj1_bucket'
 bucket = storage_client.bucket(bucket_name)
 
-def download_json_from_gcs(bucket_name, json_filename):
+def download_json_from_gcs(bucket_name, survey_folder):
     bucket = storage_client.bucket(bucket_name)
+    json_filename = f"{survey_folder}\expressions_processed.json"  # Ajustare separator pentru compatibilitate
     blob = bucket.blob(json_filename)
     data = blob.download_as_text()
     return json.loads(data)
 
-json_filename = "expressions.json"
-data = download_json_from_gcs(bucket_name, json_filename)
+# Obținerea parametrilor din URL
+query_params = st.query_params
+selected_survey = query_params["survey"]
 
-st.title("Interpretarea Creativă a Metaforelor Lingvistice prin Modele Multimodale")
+if selected_survey:
+    try:
+        data = download_json_from_gcs(bucket_name, selected_survey)
 
-descriere = """
-Scopul acestui site este de a realiza un sondaj de opinie pentru a valida dacă imaginile generate de mine sunt mai bune decât cele dintr-un set de date existent. 
-Contextul acestei lucrări de licență este crearea unui model multimodal care primește ca input o metaforă și produce o imagine ce ilustrează sensul acesteia. 
-Setul de date existent conține imagini generate care reprezintă doar metafora în mod concret, nu și sensul ei profund. 
-Acest site este creat pentru a evalua dacă imaginile generate de mine pentru un set minimal de metafore sunt mai bune decât cele din setul de date existent.
-"""
-st.write(descriere)
+        st.title("Interpretarea Creativă a Metaforelor Lingvistice prin Modele Multimodale")
 
-# CSS pentru alinierea radio button-urilor
-st.markdown("""
-    <style>
-        .stRadio [role=radiogroup]{
-            align-items: center;
-            justify-content: center;
-        }
-    </style>
-""", unsafe_allow_html=True)
+        descriere = """
+        Scopul acestui site este de a realiza un sondaj de opinie pentru a valida dacă imaginile generate de mine sunt mai bune decât cele dintr-un set de date existent. 
+        Contextul acestei lucrări de licență este crearea unui model multimodal care primește ca input o metaforă și produce o imagine ce ilustrează sensul acesteia. 
+        Setul de date existent conține imagini generate care reprezintă doar metafora în mod concret, nu și sensul ei profund. 
+        Acest site este creat pentru a evalua dacă imaginile generate de mine pentru un set minimal de metafore sunt mai bune decât cele din setul de date existent.
+        """
+        st.write(descriere)
 
-# Dicționar pentru a stoca răspunsurile utilizatorilor
-if "responses" not in st.session_state:
-    st.session_state.responses = {}
+        # CSS pentru alinierea radio button-urilor
+        st.markdown("""
+            <style>
+                .stRadio [role=radiogroup]{
+                    align-items: center;
+                    justify-content: center;
+                }
+            </style>
+        """, unsafe_allow_html=True)
 
-if "initial_images" not in st.session_state:
-    st.session_state.initial_images = {}
+        # Dicționar pentru a stoca răspunsurile utilizatorilor
+        if "responses" not in st.session_state:
+            st.session_state.responses = {}
 
-# Funcție pentru afișarea imaginilor și radio button-urilor
-def afiseaza_metafore(metafore_imagini):
-    for idx, metafora in enumerate(metafore_imagini):
-        st.subheader(metafora["text"])
+        if "initial_images" not in st.session_state:
+            st.session_state.initial_images = {}
 
-        if metafora["text"] not in st.session_state.initial_images:
-            imagini_random = list(metafora["image_urls"])
-            random.shuffle(imagini_random)
-            st.session_state.initial_images[metafora["text"]] = imagini_random
-        else:
-            imagini_random = st.session_state.initial_images[metafora["text"]]
+        # Funcție pentru afișarea imaginilor și radio button-urilor
+        def afiseaza_metafore(metafore_imagini):
+            for idx, metafora in enumerate(metafore_imagini):
+                st.subheader(metafora["text"])
 
-        num_imagini = len(imagini_random)
-        options = [f"Imagine {i + 1}" for i in range(num_imagini)] + ["Toate", "Niciuna"]
+                if metafora["text"] not in st.session_state.initial_images:
+                    imagini_random = list(metafora["image_urls"])
+                    random.shuffle(imagini_random)
+                    st.session_state.initial_images[metafora["text"]] = imagini_random
+                else:
+                    imagini_random = st.session_state.initial_images[metafora["text"]]
 
-        col = st.columns(num_imagini)
+                num_imagini = len(imagini_random)
+                options = [f"Imagine {i + 1}" for i in range(num_imagini)] + ["Toate", "Niciuna"]
 
-        label_to_url = {f"Imagine {idx + 1}": img for idx, img in enumerate(imagini_random)}
+                col = st.columns(num_imagini)
 
-        for idx, img in enumerate(imagini_random):
-            with col[idx]:
-                st.image(img, use_column_width=True)
+                label_to_url = {f"Imagine {idx + 1}": img for idx, img in enumerate(imagini_random)}
 
-        selected_option = st.radio('Selecteaza o optiune', options=options, horizontal=True,
-                                   key=f'radio_{metafora["text"]}', index=None)
+                for idx, img in enumerate(imagini_random):
+                    with col[idx]:
+                        st.image(img, use_column_width=True)
 
-        if selected_option == "Toate":
-            st.session_state.responses[metafora["text"]] = imagini_random
-        elif selected_option == "Niciuna":
-            st.session_state.responses[metafora["text"]] = []
-        elif selected_option is not None:
-            st.session_state.responses[metafora["text"]] = [label_to_url[selected_option]]
+                selected_option = st.radio('Selecteaza o optiune', options=options, horizontal=True,
+                                           key=f'radio_{metafora["text"]}', index=None)
 
-# Afișarea metaforelor cu imaginile și radio button-urile
-afiseaza_metafore(data)
+                if selected_option == "Toate":
+                    st.session_state.responses[metafora["text"]] = imagini_random
+                elif selected_option == "Niciuna":
+                    st.session_state.responses[metafora["text"]] = []
+                elif selected_option is not None:
+                    st.session_state.responses[metafora["text"]] = [label_to_url[selected_option]]
 
-# Buton pentru trimiterea răspunsurilor
-if st.button('Trimite răspunsurile'):
-    # Generarea unui timestamp pentru unicitatea numelui fișierului
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    responses_filename = f"responses_{timestamp}.json"
+        # Afișarea metaforelor cu imaginile și radio button-urile
+        afiseaza_metafore(data)
 
-    # Salvarea răspunsurilor într-un fișier JSON
-    with open(responses_filename, "w") as responses_file:
-        json.dump(st.session_state.responses, responses_file)
+        # Buton pentru trimiterea răspunsurilor
+        if st.button('Trimite răspunsurile'):
+            # Generarea unui timestamp pentru unicitatea numelui fișierului
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            responses_filename = f"responses_{selected_survey}_{timestamp}.json"
 
-    # Încărcarea fișierului JSON în Google Cloud Storage
-    responses_blob = bucket.blob(f"responses/{responses_filename}")
-    responses_blob.upload_from_filename(responses_filename)
-    responses_blob.make_public()  # Opțional: face fișierul JSON public
+            # Salvarea răspunsurilor într-un fișier JSON
+            with open(responses_filename, "w") as responses_file:
+                json.dump(st.session_state.responses, responses_file)
 
-    st.write("Răspunsurile au fost trimise. Mulțumim pentru participare!")
+            # Încărcarea fișierului JSON în Google Cloud Storage
+            responses_blob = bucket.blob(f"responses/{responses_filename}")
+            responses_blob.upload_from_filename(responses_filename)
+            responses_blob.make_public()  # Opțional: face fișierul JSON public
+
+            st.write("Răspunsurile au fost trimise. Mulțumim pentru participare!")
+    except Exception as e:
+        st.error(f"A apărut o eroare la încărcarea sondajului: {e}")
+else:
+    st.write("Nu a fost specificat niciun sondaj. Te rugăm să accesezi link-ul corect.")
